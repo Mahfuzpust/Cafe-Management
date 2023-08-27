7a4a8033b0f41842b9f5dc0529d359b67e302d4d
mahfuz khan <mahfuzpust@gmail.com>
2023-08-28 00:16:10 +0600

    Category-Index Bootwatch-for-Fontend

--- Cafe.Management/Controllers/CategoryController.cs
+++ Cafe.Management/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Cafe.Management.Data;
+using Cafe.Management.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Cafe.Management.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
