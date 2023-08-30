df4a78b851c33c46c5b03710d6e28166f4153b0a
mahfuz khan <mahfuzpust@gmail.com>
2023-08-30 23:35:06 +0600

    IUnitOfWork-UnitOfWork

---
+++ Cafe.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using Cafe.DataAccess.Data;
+using Cafe.DataAccess.Repository.IRepository;
+using Cafe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        public ICategoryRepository Category { get; private set; }
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
