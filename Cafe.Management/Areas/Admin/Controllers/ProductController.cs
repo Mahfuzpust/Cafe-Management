c5e4651389abf01bf29771db475b1f72fbf351f5
mahfuz khan <mahfuzpust@gmail.com>
2023-08-31 13:08:52 +0600

    Product-Controller

---
+++ Cafe.Management/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,102 @@
+﻿using Cafe.DataAccess.Repository.IRepository;
+using Cafe.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cafe.Management.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Product> productList = _unitOfWork.Product.GetAll().ToList();
+            return View(productList);
+        }
+        //Create GET Method
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Create POST Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product Created Successfully";
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        //Edit GET Method
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product product = _unitOfWork.Product.Get(c => c.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        //Edit POST Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product Edited Successfully";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        //Delete GET Method
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product product = _unitOfWork.Product.Get(u => u.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        //Delete POST Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //[ActionName("Delete")]
+        public IActionResult Delete(int? id, Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Remove(product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product Deleted Successfully";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+    }
+}
