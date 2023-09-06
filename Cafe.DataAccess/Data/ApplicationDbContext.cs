6d2f2f75355a925365c49483ce9ecb7931a21cd5
mahfuz khan <mahfuzpust@gmail.com>
2023-09-06 12:36:29 +0600

    Add Extend Identity User

--- Cafe.DataAccess/Data/ApplicationDbContext.cs
+++ Cafe.DataAccess/Data/ApplicationDbContext.cs
@@ -15,7 +15,7 @@ namespace Cafe.DataAccess.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
-
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
