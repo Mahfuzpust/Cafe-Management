b80f88a78ed61a8b39db16ebdc27af665f1fb1cc
mahfuz khan <mahfuzpust@gmail.com>
2023-09-07 11:59:12 +0600

    Create Role In Database

--- Cafe.Management/Program.cs
+++ Cafe.Management/Program.cs
@@ -3,6 +3,8 @@ using Cafe.DataAccess.Repository;
 using Cafe.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Cafe.Utility;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,13 +16,14 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddRazorPages();
 
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
+builder.Services.AddScoped<IEmailSender, EmailSender>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
