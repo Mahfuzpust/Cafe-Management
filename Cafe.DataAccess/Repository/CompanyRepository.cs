7447eb9b128f15b22ce07fe964fa30d9af01bca7
mahfuz khan <mahfuzpust@gmail.com>
2023-09-07 23:30:41 +0600

    Company

---
+++ Cafe.DataAccess/Repository/CompanyRepository.cs
@@ -0,0 +1,26 @@
+﻿using Cafe.DataAccess.Data;
+using Cafe.DataAccess.Repository.IRepository;
+using Cafe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe.DataAccess.Repository
+{
+    public class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CompanyRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Company obj)
+        {
+            _db.Companies.Update(obj);
+        }
+    }
+}
--- Cafe.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ Cafe.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -10,6 +10,7 @@ namespace Cafe.DataAccess.Repository.IRepository
     {
         ICategoryRepository Category { get; }
         IProductRepository Product { get; }
+        ICompanyRepository Company { get; }
         void Save();
     }
 }
