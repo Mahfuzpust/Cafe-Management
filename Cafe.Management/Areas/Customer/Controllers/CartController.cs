0311e1794dda004ded20ab7f83644c5a7d5273ca
mahfuz khan <mahfuzpust@gmail.com>
2023-09-09 12:02:47 +0600

    Shopping Cart UI

---
+++ Cafe.Management/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cafe.Management.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
